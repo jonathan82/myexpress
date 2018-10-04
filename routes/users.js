6f44a7c10c2c21eb0d3f2e22f85182a745427f96
Lin <I3083@ftb.ca.gov>
2018-10-04 15:28:05 -0700

    ok

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const Joi = require('joi');
 const router = express.Router();
+const crypto = require('crypto');
 
-const schema = {
-
-}
+const schema = Joi.object().keys({
+  username: Joi.string().required().min(3).max(25).token(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().length(10).required(),
+  password: Joi.string().required().min(6).allow(),
+  profileText: ''  
+});
 
 // Get specific user
 router.get('/:id', function(req, res, next) {
